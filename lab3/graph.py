83f0bba9e65ce4ebea11a49d88df21095eeba9fa
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-11-03 20:07:08 +0300

    Add matrix with coefs output as a graph

---
+++ lab3/graph.py
@@ -0,0 +1,31 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+EPS = 1e-3
+
+
+def convert_matrix_to_nx_form(matr):
+    res = []
+    for i_row in range(len(matr)):
+        for i_col in range(len(matr[i_row])):
+            cur = matr[i_row][i_col]
+            if cur > EPS:
+                res.append((i_row, i_col, cur))
+    return res
+
+
+def graph(matr):
+    DG = nx.DiGraph()
+    DG.add_weighted_edges_from(convert_matrix_to_nx_form(matr))
+    pos=nx.planar_layout(DG)
+    nx.draw(DG, pos, with_labels=True)
+    labels = nx.get_edge_attributes(DG, 'weight')
+    nx.draw_networkx_edge_labels(DG, pos, edge_labels=labels, label_pos=0.3)
+    plt.show()
+
+
+if __name__ == '__main__':
+    m1 = [[0, 2, 0, 3, 8], [0, 0, 3, 0, 0], [0, 4, 0, 0, 0], [0, 0, 5, 0, 2], [7, 0, 4, 0, 0]]
+    m2 = [[0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 3], [3, 0, 0, 0]]
+    m3 = [[0, 0.7525, 0.2761], [0.1805, 0., 0.3038], [0.536,  0.9106, 0.]]
+    graph(m3)
